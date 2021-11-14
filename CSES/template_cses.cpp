21a91e31058ae52b9046f72e478f8a60c45e7e75
Aleksi Kangas <aleksi.kangas@hotmail.com>
2021-11-14 17:39:14 +0200

    Template for CSES Problem Set.

---
+++ CSES/template_cses.cpp
@@ -0,0 +1,34 @@
+#include <algorithm>
+#include <bitset>
+#include <climits>
+#include <cmath>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+using ll = long long;
+
+void Solve() {
+
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  Solve();
+}
