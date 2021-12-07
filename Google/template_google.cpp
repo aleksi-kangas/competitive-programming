4555969174e6ddda8b8f3f88a4dd33014d889ae1
Aleksi Kangas <aleksi.kangas@hotmail.com>
2021-12-07 13:35:57 +0200

    Template for Google's competitions.

---
+++ Google/template_google.cpp
@@ -0,0 +1,40 @@
+#include <algorithm>
+#include <bitset>
+#include <climits>
+#include <cmath>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+using ll = long long;
+
+int Solve() {
+
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int test_cases;
+  cin >> test_cases;
+
+  for (int test_case = 1; test_case <= test_cases; ++test_case) {
+    int answer = Solve();
+    cout << "Case #" << test_case << ": " << answer << endl;
+  }
+}
